7c2f779c4153aefc1985f19c48a722db59b71080
Anmol-1-cloud <anmolsadhav11@gmail.com>
2025-08-28 14:01:21 +0200

    Add files via upload

---
+++ js_main.js
@@ -0,0 +1,3 @@
+// Add any interactivity you want here (e.g. navbar toggling, smooth scrolling, etc.)
+// For now, this file is just a placeholder.
+console.log("Welcome to your portfolio & blog site!");
